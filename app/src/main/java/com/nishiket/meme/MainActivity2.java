68659499271bcf4725a69e1cc06b66009f8e4df8
Nishiket Bhatt <nishiket04@gamil.com>
2023-07-22 12:14:40 +0530

    add clear cache method

--- app/src/main/java/com/nishiket/meme/MainActivity2.java
+++ app/src/main/java/com/nishiket/meme/MainActivity2.java
@@ -2,12 +2,15 @@ package com.nishiket.meme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity2 extends AppCompatActivity {
     Animation pop;
@@ -28,9 +31,32 @@ public class MainActivity2 extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                // get the cache file and call delete method
+                File dir = MainActivity2.this.getCacheDir();
+                deleteDir(dir);
+                // go to the new activity
                 startActivity(go);
                 finish();
             }
         },3000);
     }
-}
+
+    // delete dir method
+    private static boolean deleteDir(File dir) {
+        if(dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+                return dir.delete();}
+            else if(dir !=null && dir.isFile()){
+                return dir.delete();
+            }
+            else{
+                return false;
+            }
+        }
+    }
